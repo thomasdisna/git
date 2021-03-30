d84235d3c64bc51208788cc255f72d1f0c2244a0
DESKTOP-ASO7VGIuser <thomasdisna0@gmail.com>
2021-03-30 10:27:04 +0530

    df

--- lib/main.dart
+++ lib/main.dart
@@ -58,7 +58,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
+       // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
